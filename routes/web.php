25c79b4561d5c4300c763d251df01d5fb90237f4
Boris-Pobeshymov <boris.pobeshymov@gmail.com>
2019-11-08 17:20:28 +0200

    delete router

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,4 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/home', 'HomeController@index')->name('home');
 });
 
-
-Route::any( '(.*)', function( $page ){
-    dd($page);
-});
-
 Route::get('/{any}', 'RedirectLinksController@checkRedirect')->where('any', '.*');
